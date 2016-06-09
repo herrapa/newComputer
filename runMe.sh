301e720215fdfbe8dcbb18822bc122622abdef63
herrapa <joakim.andersson@2xper.se>
2016-06-09 09:42:42 +0200

    add more stuff

--- runMe.sh
+++ runMe.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+sudo gem install cocoapods
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 xcode-select --install
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
